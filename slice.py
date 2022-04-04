6d5f8dce702fa590b99723d5203685ab71d8ddc0
Дмитрий Пл <pldimitry@yandex.ru>
2022-04-04 17:47:59 +0300

    slice_v2

--- slice.py
+++ slice.py
@@ -1,12 +1,16 @@
+import logging
+import os
 import re
 import sys
-import os
-import logging
+import subprocess
 
 
 class Slice:
 
     def __init__(self, path=None):
+        self.dir_name = None
+        self.sample_code = None
+        self.solution = None
         if path:
             self.run_path(path)
         else:
@@ -19,9 +23,12 @@ class Slice:
 
     def run_all(self):
         for dir_name in self.get_files():
+            self.dir_name = dir_name
             file = self.get_main_py(dir_name)
             if not file: continue
             files = self.slice(file.split('\n'))
+            self.checked_comment()
+            self.checked_test()
             self.write_files(dir_name, files)
 
     def get_main_py(self, path: str):
@@ -32,7 +39,8 @@ class Slice:
         print(f'{path}/main.py does not exist')
 
     def get_files(self):
-        files_path = [i for i in os.listdir() if not re.match(r'.*\..*', string=i) and re.match(r'^\d.*', string=i)]
+        files_path = [i for i in sorted(os.listdir()) if
+                      not re.match(r'.*\..*', string=i) and re.match(r'^\d.*', string=i)]
         return files_path
 
     def slice(self, file):
@@ -48,8 +56,9 @@ class Slice:
             try:
                 with open(f'{path}/{name}', 'w') as f:
                     f.write(file)
+                subprocess.call(['python3.8', '-m', 'black', f'{self.dir_name}/{name}'])
             except TypeError:
-                logging.warning(f'ОШИБКА ЧТЕНИЯ ФАЙЛА {path+"/main.py"}')
+                logging.warning(f'ОШИБКА ЧТЕНИЯ ФАЙЛА {path + "/main.py"}')
 
     def get_code(self, file, target, line_skip=1, is_end=False):
         start = None
@@ -59,7 +68,31 @@ class Slice:
                 if is_end:
                     return '\n'.join(file[index + line_skip:])
             if start and index > start and i == "'''":
+                if target == "# sample_code":
+                    self.sample_code = file[start:index]
+                elif target == "# solution":
+                    self.solution = file[start:index]
                 return '\n'.join(file[start:index])
+
+    def checked_comment(self):
+        for sam, sol in zip(self.sample_code, self.solution):
+            if (re.search(r'#\s*', sam) and not re.search(r'\--$', sam)) \
+                    and (re.search(r'#\s*', sol) and not re.search(r'\--$', sol)):
+                res = [sam, sol]
+                if len(set(res)) > 1:
+                    print(f"\n Комментарий не совпадает: {self.dir_name} - {set(res)}\n")
+                    breakpoint()
+                elif re.search(r'#\s*[а-я]|#\s*[а-я]\s*\.|\.$', sam):
+                    print(
+                        f"\n Комментарий с маленькой буквы или точка в конце: {self.dir_name} - {sam}\n")
+                    breakpoint()
+
+    def checked_test(self):
+        print(f"Тест: {self.dir_name}/main.py")
+        proc = subprocess.call(['pytest', f'{self.dir_name}/main.py'])
+        if proc == 2:
+            print(f"\n Ошибка ожиданий: {self.dir_name}\n")
+            breakpoint()
 
 
 if __name__ == "__main__":
